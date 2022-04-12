c527f2163a31bb8cf2ec6b682f8094d124732304
Rohit <rohit200827@gmail.com>
2022-04-12 16:17:12 +0530

    push

---
+++ variables.py
@@ -0,0 +1,14 @@
+myint = 7; # this is a variable , now les c types in python 👀
+
+# above is a variable with the type number and value of 7
+
+print(myint)
+# prints 7
+
+# Floats are numbers with decimals
+# lemme c how it works 👀
+myfloat = 7.0
+# myfloat is a variable with the type float and a value of 7.0
+print(myfloat)
+# prints 7.0
+
